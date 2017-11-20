b9c15e69600459687d2325cdd2cdbf091f8da1bf
deadsik <deadsik@ukr.net>
2017-11-20 16:48:58 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ if [ -n "$CHECK_IFCONFIG" ]; then
 
 mysql --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWD $DB_NAME << EOF
 UPDATE $DB_NAME.realmlist SET name='DARK-GAMES.ORG.UA', address='$CHECK_IFCONFIG', port='8085' WHERE id=1;
-INSERT INTO $DB_NAME.account (username, sha_pass_hash) VALUES ('deadsik', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+INSERT INTO $DB_NAME.account (username, sha_pass_hash) VALUES ('admin', 'd13fcd1b568d81a7f4d55ec614dfa4b3d21641a2');
 INSERT INTO $DB_NAME.account_access (id, gmlevel, RealmID) VALUES (1, 3, 1);
 EOF
 
