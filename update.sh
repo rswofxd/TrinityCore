373ae03935862c6c52506f06d104f72497f13a73
deadsik <deadsik@ukr.net>
2017-11-20 17:23:06 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,17 +8,18 @@ DB_PASSWD="trinity"
 DB_NAME="auth"
 
 LOGIN="admin"
+EMAIL="evgeniy@kolesnyk.ru"
 PASSWORD="1234"
-SHA1_PASSWORD=`echo -n "$LOGIN:$PASSWORD" | tr [:lower:] [:upper:] | sha1sum | awk '{print $1}'`
+REALMLIST="DARK-GAMES.ORG.UA"
+
 
 if [ -n "$CHECK_IFCONFIG" ]; then
-
+SHA1_PASSWORD=`echo -n "$LOGIN:$PASSWORD" | tr [:lower:] [:upper:] | sha1sum | awk '{print $1}'`
 mysql --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWD $DB_NAME << EOF
-UPDATE $DB_NAME.realmlist SET name='DARK-GAMES.ORG.UA', address='$CHECK_IFCONFIG', port='8085' WHERE id=1;
-INSERT INTO $DB_NAME.account (username, sha_pass_hash) VALUES ('$LOGIN', '$SHA1_PASSWORD');
+UPDATE $DB_NAME.realmlist SET name='$REALMLIST', address='$CHECK_IFCONFIG', port='8085' WHERE id=1;
+INSERT INTO $DB_NAME.account (username, email, sha_pass_hash) VALUES ('$LOGIN', '$EMAIL', '$SHA1_PASSWORD');
 INSERT INTO $DB_NAME.account_access (id, gmlevel, RealmID) VALUES (1, 3, 1);
 EOF
-
 sed -i '/update.sh/d' /root/restart_all.sh
 rm -f /root/update.sh
 fi
