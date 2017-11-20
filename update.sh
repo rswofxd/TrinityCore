c12298a757edd042c4ec9f8a60a65da67692622d
deadsik <deadsik@ukr.net>
2017-11-20 17:13:30 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,11 +7,15 @@ DB_USER="trinity"
 DB_PASSWD="trinity"
 DB_NAME="auth"
 
+LOGIN="admin"
+PASSWORD="1234"
+SHA1_PASSWORD=`echo -n "$LOGIN:$PASSWORD" | tr [:lower:] [:upper:] | sha1sum | awk '{print $1}'`
+
 if [ -n "$CHECK_IFCONFIG" ]; then
 
 mysql --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWD $DB_NAME << EOF
 UPDATE $DB_NAME.realmlist SET name='DARK-GAMES.ORG.UA', address='$CHECK_IFCONFIG', port='8085' WHERE id=1;
-INSERT INTO $DB_NAME.account (username, sha_pass_hash) VALUES ('admin', '00a51843d4c99be8b358ff288ea014525b0445c3');
+INSERT INTO $DB_NAME.account (username, sha_pass_hash) VALUES ('$LOGIN', '$SHA1_PASSWORD');
 INSERT INTO $DB_NAME.account_access (id, gmlevel, RealmID) VALUES (1, 3, 1);
 EOF
 
