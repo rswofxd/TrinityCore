d97fb6d601194fd4df4cb13e2db7b99733ae5ecf
brouzuf <brouzuf@gmail.com>
2019-10-08 16:50:46 +0200

    Create InstallDatabase.sh

---
+++ InstallDatabase.sh
@@ -0,0 +1,8 @@
+
+
+
+
+    mysql < /home/server/source/TrinityCore/sql/create/create_mysql.sql && \
+    mysql auth < /home/server/source/TrinityCore/sql/base/auth_database.sql && \
+    mysql characters < /home/server/source/TrinityCore/sql/base/characters_database.sql && \
+    mysql world < /home/server/source/TrinityCore/sql/base/TDB_full_world_335.19071_2019_07_15.sql
