94945ffab346e7fc5459bf5b742b9ab59a03fa76
brouzuf <brouzuf@gmail.com>
2019-10-18 17:13:16 +0200

    Update InstallTrinity.sh

--- InstallTrinity.sh
+++ InstallTrinity.sh
@@ -1,2 +1,7 @@
-cd /source
+cd /home/server/source
+echo "Pull latest code in Github..."
 git pull origin 3.3.5
+cd /home/server/source/TrinityCore/build
+echo "Build !!"
+cmake ../ -DCMAKE_INSTALL_PREFIX=/home/server/wow -DCONF_DIR=/home/server/wow/conf -DTOOLS=1
+make && make install
