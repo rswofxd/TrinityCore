70185b20ca328628449bd22462320a721017be3f
brouzuf <brouzuf@gmail.com>
2019-10-29 16:03:55 +0100

    Delete restart_authserver.sh

--- restart_authserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for i in `screen -ls |grep 'authserver' |awk '{print $1}'` ;  do screen -X -S $i quit ; done
-screen -A -m -d -S authserver sudo -H -u server bash -c 'cd /home/server/wow/bin  && /home/server/wow/bin/authserver'
